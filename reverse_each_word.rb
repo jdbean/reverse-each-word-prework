bc86ece6ca024c38786abbdc8da12dee21caa535
J.D. Bean <jonathandbean@gmail.com>
2018-02-16 00:14:24 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  reversed_array = []
+  array.each do |word|
+    letter_array = word.split("")
+    reversed_array << letter_array.reverse
+  end
+  return reverse_array
+end
+
+
+reverse_each_word("hey there buddy!")
